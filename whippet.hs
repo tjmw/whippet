874b04520cf182e2cdcd78f49f29590ca7d0f1ee
Tom Wey <tjmwey@gmail.com>
2014-11-02 14:19:16 +0000

    Support for inexact matches
    Just output this list separately for now (also includes duplicates).

--- whippet.hs
+++ whippet.hs
@@ -1,3 +1,5 @@
+import Data.List
+import Data.List.Split
 import Data.Maybe
 import System.Directory
 import System.Environment
@@ -8,6 +10,10 @@ exactMatches :: [Char] -> [[Char]] -> [[Char]]
 exactMatches query strings = [s | s <- strings, not $ isNothing $ match queryRegex s]
   where queryRegex = stringToRegex query
 
+inexactMatches :: [Char] -> [[Char]] -> [[Char]]
+inexactMatches query strings = [s | s <- strings, not $ isNothing $ match queryRegex s]
+  where queryRegex = stringToFuzzyRegex query
+
 match :: Regex -> [Char] -> Maybe[String]
 match query string = matchRegex query string
 
@@ -17,7 +23,13 @@ caseSensitive = False
 stringToRegex :: [Char] -> Regex
 stringToRegex string = mkRegexWithOpts string notMultiline caseSensitive
 
+stringToFuzzyRegex :: [Char] -> Regex
+stringToFuzzyRegex string = mkRegexWithOpts (intercalate ".*" (splitOn "" string)) notMultiline caseSensitive
+
 main = do
   [query, directory] <- getArgs
   files              <- getDirectoryContents directory
+  putStrLn "Exact:"
   putStr (unlines (exactMatches query files))
+  putStrLn "Inexact:"
+  putStr (unlines (inexactMatches query files))
