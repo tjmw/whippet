eb06c45096177b80a1b73d4d630b65aa0049be73
Tom Wey <tom@new-bamboo.co.uk>
2014-12-11 15:30:05 +0000

    Parse command line options with `options` package

--- library/Whippet.hs
+++ library/Whippet.hs
@@ -1,13 +1,28 @@
 module Whippet (run, removeDups, exactMatches, inexactMatches) where
 
+import Control.Applicative
 import Control.Monad (forM)
 import Data.List (intercalate)
 import Data.List.Split (splitOn)
 import Data.Maybe (isNothing)
+import Data.Strings (strNull)
+import Options
 import System.Directory (doesDirectoryExist, getDirectoryContents)
 import System.Environment (getArgs)
 import System.FilePath ((</>))
 import Text.Regex (Regex, matchRegex, mkRegexWithOpts)
+
+data MainOptions = MainOptions {
+  optPath   :: String,
+  optQuery  :: String
+}
+
+instance Options MainOptions where
+	defineOptions = pure MainOptions
+		<*> simpleOption "path" "."
+		    "Path to search from"
+		<*> simpleOption "query" ""
+		    "Search query"
 
 matches :: Regex -> [[Char]] -> [[Char]]
 matches regex strings = [s | s <- strings, not $ isNothing $ match regex s]
@@ -46,7 +61,11 @@ getRecursiveDirectoryContents topdir = do
   return (concat paths)
 
 run :: IO ()
-run = do
-  [query, directory] <- getArgs
-  files              <- getRecursiveDirectoryContents directory
-  putStr (unlines (removeDups (exactMatches query files) (inexactMatches query files)))
+run = runCommand $ \opts args -> do
+  let query = optQuery opts
+  let path = optPath opts
+  if (strNull query)
+    then putStr ""
+    else do
+      files <- getRecursiveDirectoryContents (path)
+      putStr (unlines (removeDups (exactMatches query files) (inexactMatches query files)))
